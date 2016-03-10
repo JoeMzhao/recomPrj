6d8a27386d6ee438b40b02d3a96c324c50a83f9c
Mengjie <Mengjie@tsf-428-wpa-0-143.epfl.ch>
2016-03-10 17:45:13 +0100

    regular updates

--- mainTimporal.m
+++ mainTimporal.m
@@ -35,7 +35,7 @@ for i   = 1:size(testSet, 1)
     testRateMat(testSet(i, 1), testSet(i, 2)) = testSet(i, 3);
 end
 
-%%
+%% define storages
 % for each user, we need to define a pool
 freq     = tabulate(sorted(:, 1));
 sizeVec  = floor(0.6 * freq(:, 2));
@@ -43,12 +43,12 @@ existVec = zeros(numUser, 1);
 
 % using the first 20000 data to train the initial two matrices
 oriTrinSet = sorted(1 : 0.05 * dataLen, :);
-comingSet  = sorted(0.2 * dataLen + 1 : 0.8 * dataLen, :);
+comingSet  = sorted(0.05 * dataLen + 1 : 0.8 * dataLen, :);
 
-buff       = tabulate(oriTrinSet(:, 1));
+buff        = tabulate(oriTrinSet(:, 1));
 existVec(1:size(buff, 1))   = buff(:, 2);
 
-trinRateMat  = zeros(numUser, numMovi);
+trinRateMat = zeros(numUser, numMovi);
 for i = 1:size(oriTrinSet, 1)
     trinRateMat(oriTrinSet(i, 1), oriTrinSet(i, 2)) = oriTrinSet(i, 3);
 end
@@ -61,8 +61,43 @@ for i = 1:length(dstctIdx)
                                                          dstctIdx(i)), :);
 end
 
+[userMat, moviMat, MAE] = getMAE(trinRateMat, testRateMat);
 
-% [userMat, moviMat, MAE] = getMAE(trinRateMat, testRateMat);
+% eliminate oldest data if number of existing rates exceed pool size
+for i = 1:numUser
+    if existVec(i) > sizeVec(i)
+        amout = existVec(i) - sizeVec(i);
+        poolAry{i}(1:amout, :) = [];
+        existVec(i) = sizeVec(i);
+    end
+end
+
+%% when a new data comes...
+%comingSet
+
+% for i = 1:size(comingSet, 1)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
