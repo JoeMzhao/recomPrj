b4b60118a0b1367eaf08103a0d5cf89df8166436
Mengjie <Mengjie@tsf-428-wpa-0-209.epfl.ch>
2016-03-17 19:13:23 +0100

    minor modifications

--- newMain.m
+++ newMain.m
@@ -22,7 +22,7 @@ numUser  = 943;
 numMovi  = 1682;
 
 maxIters  = 500;
-M         = 10;
+M         = 20;
 regular_u = 1.1;
 regular_m = 0.34;
 tolerence = 1e-6;
@@ -36,20 +36,21 @@ for i   = 1:size(testSet, 1)
 end
 
 %% define storages
-% for each user, we need to define a pool
-freq     = tabulate(sorted(:, 1));
-sizeVec  = floor(0.6 * freq(:, 2));
-order    = sizeVec + 5;
-
 % using the first part of data to train the initial two matrices
-oriTrinSet = sorted(1 : 0.05 * dataLen, :);
-comingSet  = sorted(0.05 * dataLen + 1 : 0.8 * dataLen, :);
+oriTrinSet = sorted(1 : 0.79 * dataLen, :);
+comingSet  = sorted(0.79 * dataLen + 1 : 0.8 * dataLen, :);
+
+% for each user, we need to define a pool
+freq     = tabulate(oriTrinSet(:, 1));
+sizeVec  = floor(0.6 * freq(:, 2));
+order    = sizeVec + 10;
 
 existVec = zeros(numUser, 1);
 buff     = tabulate(oriTrinSet(:, 1));
 existVec(1:size(buff, 1))   = buff(:, 2);
 
 trinRateMat = zeros(numUser, numMovi);
+
 for i = 1:size(oriTrinSet, 1)
     trinRateMat(oriTrinSet(i, 1), oriTrinSet(i, 2)) = oriTrinSet(i, 3);
 end
@@ -63,13 +64,8 @@ for i = 1:length(dstctIdx)
 end
 
 [userMat, moviMat, MAE] = getMAE(trinRateMat, testRateMat);
-kkk = userMat;
-jjj = moviMat;
 MAE
-
-% eliminate oldest data if number of existing rates exceed pool size
-% the original data is used in training the original 2 matrices
-% is it ok to delete them?
+copy_of_moviMat = cell(numUser, 1);
 
 for i = 1:numUser
     if existVec(i) > sizeVec(i)
@@ -77,15 +73,17 @@ for i = 1:numUser
         poolAry{i}(1:amout, :) = [];
         existVec(i) = sizeVec(i);
     end
+    copy_of_moviMat{i, 1} = moviMat;
 end
+
 
 %% when a new data comes...
 %comingSet
 global alpha beta T
 
-alpha  = 1e-5;
-beta   = 1e-5;
-T      = 5;
+alpha  = 1e-4;
+beta   = 1e-4;
+T      = 1;
 unUseC = 0;
 inserC = 0;
 
@@ -95,35 +93,38 @@ for i = 1:size(comingSet, 1)
     diceIn  = rand(1);
     uidx    = comingSet(i, 1);
     midx    = comingSet(i, 2);
-    addIdx  = 1;
       
-      if diceIn > 0 %<= ( 1 - sizeVec(uidx) / order(uidx) )
-          inserC = inserC + 1;
+      if diceIn <= ( 1 - sizeVec(uidx) / order(uidx) )
+            inserC = inserC + 1;
             order(uidx) = order(uidx) + 1;
-%             diceOut = 
-            poolAry{uidx}(addIdx, :) = comingSet(i, :);
-            addIdx  = addIdx + 1;
-%             disp('updated!')
-            
+            poolAry{uidx}(end+1, :) = comingSet(i, :);
       end
        
     if ~isempty(poolAry{uidx})
-      for iter = 1: T
-         [Wu, Hp] = increUpdate(poolAry{uidx}, userMat, moviMat, userMat(:, uidx));
+      
+        for iter = 1: T
+         [Wu, Hp] = increUpdate(poolAry{uidx}, userMat, copy_of_moviMat{uidx}, userMat(:, uidx));
          
          userMat(:, uidx) = Wu;
-         moviMat = Hp;
-%          disp('ALGO used!')
-      end
+         copy_of_moviMat{uidx} = Hp;
+        end
+      
     else
         unUseC = unUseC + 1;
         continue;
     end
 end
 
-pred = userMat' * moviMat;
+ovaRates = zeros(numUser, numMovi);
+kkk      = userMat';
+for i = 1:numUser
+    for j = 1:numMovi
+        ovaRates(i, j) = kkk(i, :) * copy_of_moviMat{i}(:, j);
+    end
+end
 
-MAE2 = computeMAE(userMat' * moviMat, testRateMat)   
+MAE2 = computeMAE(ovaRates, testRateMat)   
+MAE3 = computeMAE(userMat'*moviMat, testRateMat)  
 unUseC
 inserC
 MAE
