869fba0b150f198cb90417f8a72e15aebe60c3ec
Mengjie <Mengjie@tsf-428-wpa-7-036.epfl.ch>
2016-03-21 14:01:56 +0100

    adding incremental updates

--- mainIncremental.m
+++ mainIncremental.m
@@ -2,11 +2,11 @@ clear; close all; clc
 
 addpath('ml-100k');
 
-global alpha beta numUser numMovi poolSize M maxIters tolerence
+global alpha beta numUser numMovi poolSize M maxIters tolerence T
 
 rawData      = load('u.data');
 sorted       = sortrows(rawData, 4);
-sorted(:, 4) = sorted(: ,4) - sorted(1, 4); 
+sorted(:, 4) = 1:size(sorted, 1); 
 dataLen      = size(sorted, 1);
 
 trainSet     = sorted(1:0.8 * dataLen, :);
@@ -14,30 +14,26 @@ testSet      = sorted(0.8 * dataLen + 1 : end, :);
 
 numUser      = 943;
 numMovi      = 1682;
-maxIters     = 500; 
-alpha        = 0.1;
-beta         = 0.1;
+maxIters     = 100; 
+alpha        = 1e-5;
+beta         = 1e-5;
 tolerence    = 1e-5; 
+T            = 10;
 
 %% Process the data and obtain rate matrices
 
 trainRateMat  = zeros(numUser, numMovi);
-trainTempMat  = zeros(numUser, numMovi);
+testRateMat   = zeros(numUser, numMovi);
 
 for i  = 1 : dataLen * 0.8
     trainRateMat(trainSet(i, 1), trainSet(i, 2)) = trainSet(i, 3);
-    trainTempMat(trainSet(i, 1), trainSet(i, 2)) = trainSet(i, 4);
 end
-
-testRateMat   = zeros(numUser, numMovi);
-testTempMat   = zeros(numUser, numMovi);
 
 for i  = 1 : size(testSet, 1)
-    testRateMat(testSet(i, 1), testSet(i, 2)) = testSet(i, 3);
-    testTempMat(testSet(i, 1), testSet(i, 2)) = testSet(i, 4);
+    testRateMat(testSet(i, 1), testSet(i, 2)) = testSet(i, 3);  
 end
 
-poolSize  = 20000;
+poolSize  = 70000;
 pool      = trainSet(1:poolSize, :);
 
 M         = 20;
@@ -46,38 +42,54 @@ moviMat   = rand(M, numMovi);
 
 %% The initial phase >> train two initial matrices
 iniPred   = userMat' * moviMat;
-% [userMat, moviMat, MAE] = getMAE(iniPred, testRateMat); disp(MAE);
-
-
+[userMat, moviMat, MAE1] = getMAE(iniPred, testRateMat); disp(MAE1);
 curPred   = userMat' * moviMat;
 
 for i = (poolSize + 1) : dataLen * 0.8
-       
-%     userDataRaw1 = pool( find(pool(:, 1) == trainSet(i, 1)), :); 
-    userDataRaw1 = pool;
-    boo          = inORnot(trainSet(i, 4))
-    if boo
-           timeArry  = userDataRaw1(:, 4);
+    i
+    userUpool1 = pool(find(pool(:, 1) == trainSet(i, 1)), :);
+        
+    if inORnot(trainSet(i, 4))
+        
+        timeArry  = pool(:, 4);
            
-           [repIdx, threshArry2] = whichOut(timeArry, trainSet(i, 4));
-           userDataRawBuff       = userDataRaw1;
+        [repIdx, threshArry2] = whichOut(timeArry, trainSet(i, 4));
+                
+        pool(repIdx, :) = trainSet(i, :); 
            
-           userDataRawBuff(repIdx, :) = trainSet(i, :); 
-           fprintf('No. %d  data point is replaced\n', repIdx);
-    end
-       
+        userUpool2 = pool(find(pool(:, 1) == trainSet(i, 1)), :);
+      
+    SPuIdx = SamplePositiveInput(curPred, userUpool1, trainSet(i, :));
+   
+    SNuIdx = SampleNegativeInput(curPred, userUpool2, SPuIdx, trainSet(i, :));
+          
+    uID = trainSet(i, 1);
+
+        for round = 1:T
+            for ii = 1:length(SPuIdx)
+                rate_hat = curPred(uID, SPuIdx(ii));
+                rate_avg = mean(curPred(uID, SNuIdx(:)));
+                ita      = max(0, rate_hat - rate_avg);
+                nega_avg = sum(moviMat(:, SNuIdx(:)), 2) ./ length(SNuIdx);
+
+                userMat(:, uID) = userMat(:, uID) + ...
+                                    alpha * ita .* (moviMat(:, ii) - nega_avg) - ...
+                                    alpha * beta .* userMat(:, uID);
+
+                moviMat(:, ii) = moviMat(:, ii) + alpha * ita .* userMat(:, uID) - ...
+                                    alpha * beta .* moviMat(:, ii);
+                  for j = 1:length(SNuIdx)
+                      moviMat(:, j) = moviMat(:, j) - alpha * ita .* userMat(:, uID) ...
+                                        - alpha * beta * moviMat(:, j);
+                  end
+            end
+        end
+    
+    end          
 end    
-            
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+nowPred = userMat' * moviMat;
+getMAE  = computeMAE(testRateMat, nowPred)
     
     
     
